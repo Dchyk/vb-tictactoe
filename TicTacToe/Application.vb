d0c95d0a4bfa1abfc355c5615a4f7c3658c5a092
Brian Burns <alex.dzwonchyk@gmail.com>
2018-12-04 15:52:46 -0500

    Complete game logic and flow control, add score keeping

--- TicTacToe/Application.vb
+++ TicTacToe/Application.vb
@@ -1,74 +1,107 @@
-﻿
+﻿'Tic Tac Toe for Visual Basic
 
 Public Class Application
     Public Shared Sub Main()
-        Dim squares(8) As String
-          
-        'Set the initial values of the squares
-        For i As Integer = 0 To 8
-            squares(i) = " "
-        Next
+    
+    Dim playAgain As String = "Y"
+    Dim totalScorePlayer As Integer = 0
+    Dim totalScoreComputer As Integer = 0
+    
+        While playAgain <> "N"
         
-        Dim playedSquare As Integer
-        Dim computerSquare As Integer
-        Dim isValidInput As Boolean = False
-        Dim computerSymbol As String = "O"
-        Dim winner As Boolean = False
-        
-        'The main game loop
-        While BoardIsFull(squares) = False
+            Dim squares(8) As String
+              
+            'Set the initial values of the squares
+            For i As Integer = 0 To 8
+                squares(i) = " "
+            Next
+            
+            Dim playedSquare As Integer
+            Dim computerSquare As Integer
+            
+            Dim isValidInput As Boolean = False
+            
+            Dim computerSymbol As String = "O"
+            Dim playerSymbol As String = "X"
+            
+            Dim someoneWon As Boolean = False
+            Dim winner As String = "Noone"
+
+            'The main game loop
+            While Not BoardIsFull(squares) And Not someoneWon
+            
+                WriteBoard(squares, totalScorePlayer, totalScoreComputer)
+                System.Console.WriteLine("You are 'X', and the Computer is 'O'.")
+                
+                While True
+                    'Use Try/Catch to validate user input as an integer              
+                    System.Console.WriteLine("Please enter the number of the square you would like to play on (1-9):")
+                    Try
+                        playedSquare = Integer.Parse(System.Console.ReadLine()) - 1 'Square 1 is index 0
+                        
+                        If SquareIsOpen(squares(playedSquare)) Then
+                            squares(playedSquare) = playerSymbol
+                            System.Console.WriteLine($"You played on square {playedSquare + 1}")
+                            PauseForEffect()
+                            'Break the while loop if the player has succesfully played a square
+                            Exit While
+                        Else 
+                            System.Console.WriteLine($"Square {playedSquare + 1} has already been played - try again!")
+                            Exit Try
+                        End If
+                    Catch
+                        System.Console.WriteLine("Invalid square - you must enter a number between 1 and 9!")
+                        System.Console.WriteLine("Press Enter to try again >>")
+                        System.Console.ReadLine()
+                    End Try
+                End While 
+                
+                If Not BoardIsFull(squares) Then
+                    computerSquare = FindOpenSquare(squares)
+                    squares(computerSquare) = computerSymbol
+                    System.Console.WriteLine($"The computer played on square {computerSquare + 1}")
+                    PauseForEffect()
+                End If
+                
+                If WinningLine(squares, computerSymbol) Then 
+                   winner = "Computer"
+                   totalScoreComputer += 1
+                   Exit While
+                Else If WinningLine(squares, playerSymbol) = True Then
+                   winner = "You"
+                   totalScorePlayer += 1
+                   Exit While
+                End If
+               
+            End While
+            
             System.Console.Clear()
+            WriteBoard(squares, totalScorePlayer, totalScoreComputer) 'Display the final board
             
-            'Write the Board
-            WriteBoard(squares)
-            
-            'User Plays
-            System.Console.WriteLine("You are 'X', and the Computer is 'O'.")
-            
-            
-            While True
-                'Use Try/Catch to validate user input as an integer              
-                System.Console.WriteLine("Please enter the number of the square you would like to play on:")
-                Try
-                    playedSquare = Integer.Parse(System.Console.ReadLine()) - 1
-                    
-                    If SquareIsOpen(squares(playedSquare)) Then
-                        squares(playedSquare) = "X"
-                        System.Console.WriteLine($"You played on square {playedSquare}") 
-                        'Break the loop if the player has succesfully played a square
-                        Exit While
-                    Else 
-                        System.Console.WriteLine($"Square {playedSquare} has already been played - try again!")
-                        Exit Try
-                    End If
-                Catch
-                    System.Console.WriteLine("Invalid square - you must enter a number between 1 and 9!")
-                    System.Console.WriteLine("Press Enter to try again >>")
-                    System.Console.ReadLine()
-                End Try
-            End While 
-            
-            If BoardIsFull(squares) <> True Then
-                'Computer plays
-                computerSquare = FindOpenSquare(squares)
-                squares(computerSquare) = "O"
-                System.Console.WriteLine($"The computer played on square {computerSquare}")
+            If winner <> "Noone" Then
+                System.Console.WriteLine($"Game Over! {winner} won!")
+            Else 
+                System.Console.Writeline("Game Over! The Board was completely filled up and noone won!")
             End If
             
-             'Check winning lines
-             If WinningLine(squares, computerSymbol) Or WinningLine(squares, "X")
-                winner = True
-                Exit While
-             End If
-        End While
+            System.Console.WriteLine("Do you want to play again? Y/N:")
+            playAgain = System.Console.ReadLine()
+        End While    
         
-        'Write the full board one last time
-        WriteBoard(squares)
-        System.Console.Writeline("Game Over! The Board was completely filled up!")  
     End Sub
     
-    Public Shared Sub WriteBoard(squares As String())
+    Public Shared Sub PauseForEffect()
+        'This raises a warning:
+        'Access of shared member, constant member, enum member or nested type through an instance
+        System.Threading.Thread.CurrentThread.Sleep(1000)
+    End Sub
+    
+    Public Shared Sub WriteBoard(squares As String(), playerTotal As Integer, computerTotal As Integer)
         Dim line As String = "-----"
+        
+        System.Console.Clear()
+        System.Console.WriteLine($"Player Wins: {playerTotal} | Computer Wins: {computerTotal}")
+        System.Console.WriteLine("")
         
         'Write the board to the console'
         System.Console.WriteLine($"{squares(0)}|{squares(1)}|{squares(2)}")
@@ -76,6 +109,7 @@ Public Class Application
         System.Console.WriteLine($"{squares(3)}|{squares(4)}|{squares(5)}")
         System.Console.WriteLine($"{line}")
         System.Console.WriteLine($"{squares(6)}|{squares(7)}|{squares(8)}")
+        System.Console.WriteLine("")
     End Sub
     
     Public Shared Function SquareIsOpen(square As String) As Boolean
@@ -95,7 +129,6 @@ Public Class Application
     Public Shared Function FindOpenSquare(squares As String()) As Integer
         'Generate a random number between 1 and 9
         'If it is open, return it. If not, keep trying
-        
         Dim found As Boolean = False
         Dim random As System.Random = new System.Random
         Dim testedSquare As Integer
@@ -112,7 +145,7 @@ Public Class Application
     End Function
     
     Public Shared Function WinningLine(squares As String(), playerSymbol As String) As Boolean
-        'Winning lines on the board:'
+        'All possible winning lines on the board:'
         '1, 2, 3'
         '4, 5, 6'
         '7, 8, 9'
@@ -122,16 +155,28 @@ Public Class Application
         '1, 5, 9'
         '3, 5, 7'
         
-        'We'll use a 2 dimensional array 
-        Dim winningLines = New Integer(7, 2) {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}}
+        Dim winningLineFound As Boolean = False
+        Dim squaresPlayedInARow As Integer
         
-        For Each line As Integer In winningLines
-            'Testing...
-            If line(0) = playerSymbol AndAlso line(1) = playerSymbol AndAlso line(2) = playerSymbol
+        'We'll use a 2 dimensional array 
+        Dim winningLines(,) As Integer = New Integer(7, 2) {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}}
+        Dim currentSquare As Integer
+        
+        For i As Integer = 0 To 7
+            squaresPlayedInARow = 0
+            For j As Integer = 0 To 2
+                currentSquare = winningLines(i, j) - 1
+                If squares(currentSquare) = playerSymbol Then
+                    squaresPlayedInARow += 1
+                End If    
+            Next
+            
+            If squaresPlayedInARow = 3 Then
                 Return True
-            End If          
-        Next 
-
+            End If
+        Next
+        
         Return False
     End Function
+    
 End Class
